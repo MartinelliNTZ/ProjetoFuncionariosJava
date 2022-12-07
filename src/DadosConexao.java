e171c629f439874395255741d8fa40b5f7b6848a
MartinelliNTZ <martinelli.matheus0@gmail.com>
2022-12-07 20:20:20 -0300

    finish him

--- src/DadosConexao.java
+++ src/DadosConexao.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
  * 07-12-2022
  */
 public class DadosConexao {
-    private static final String USERNAME = "acerV3";
-    private static final String PASSWORD = "mysql";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "12345678";
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/banco_funcionarios";
     public static Connection criarConexao() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
